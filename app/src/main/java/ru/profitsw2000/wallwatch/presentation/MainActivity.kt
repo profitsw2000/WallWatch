d6c5005c48222ff5ed286ba5a0a3e9e2c837a9a0
profitsw2000 <profitsw2000@gmail.com>
2025-03-17 20:32:41 +0300

    Базовый коммит.

---
+++ app/src/main/java/ru/profitsw2000/wallwatch/presentation/MainActivity.kt
@@ -0,0 +1,24 @@
+package ru.profitsw2000.wallwatch.presentation
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import ru.profitsw2000.wallwatch.R
+import ru.profitsw2000.wallwatch.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
