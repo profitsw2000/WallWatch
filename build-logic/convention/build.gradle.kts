1fa6aae77efb4d99ce15fffce610c52b810d0808
profitsw2000 <profitsw2000@gmail.com>
2025-03-18 22:59:20 +0300

    В build.gradle.kts модуля convention зарегистрированы кастомные плагины.

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -9,6 +9,13 @@ dependencies {
 
 gradlePlugin {
     plugins {
-
+        register("mainApplicationGradlePlugin") {
+            id = libs.plugins.ww.main.app.gradle.plugin.get().pluginId
+            implementationClass = "MainApplicationGradlePlugin"
+        }
+        register("mainLibraryGradlePlugin") {
+            id = libs.plugins.ww.main.lib.gradle.plugin.get().pluginId
+            implementationClass = "MainLibraryGradlePlugin"
+        }
     }
 }
