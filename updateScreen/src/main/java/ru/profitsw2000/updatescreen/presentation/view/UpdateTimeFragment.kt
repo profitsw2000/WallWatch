22548ab14ff07e715bf279fc1739d9ed1816f5c3
profitsw2000 <profitsw2000@gmail.com>
2025-03-29 22:55:40 +0300

    Сделана проверка разрешения на работу с блютуз для версий андроида старше 30 (Андроид 12 и выше), включение(выключение) и отображение соответствующей иконки блютуз в меню в зависимости от того, включён блютуз или нет (синяя или серая иконка).

--- updateScreen/src/main/java/ru/profitsw2000/updatescreen/presentation/view/UpdateTimeFragment.kt
+++ updateScreen/src/main/java/ru/profitsw2000/updatescreen/presentation/view/UpdateTimeFragment.kt
@@ -1,8 +1,15 @@
 package ru.profitsw2000.updatescreen.presentation.view
 
-import android.graphics.Typeface
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
@@ -10,6 +17,10 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import ru.profitsw2000.updatescreen.R
 import ru.profitsw2000.updatescreen.databinding.FragmentUpdateTimeBinding
 
@@ -19,6 +30,22 @@ class UpdateTimeFragment : Fragment() {
     private val binding get() = _binding!!
     private var bluetoothIsOn = false
     private var isConnected = false
+    private val bluetoothManager: BluetoothManager by lazy {
+        requireActivity().getSystemService(BluetoothManager::class.java)
+    }
+    private val bluetoothAdapter: BluetoothAdapter by lazy {
+        bluetoothManager.adapter
+    }
+    private val requestCodeForEnable = 1
+    val requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                switchBluetooth()
+            } else {
+                showRationaleDialog()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,22 +65,28 @@ class UpdateTimeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
     }
 
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.menu_update_time, menu)
+        menu.findItem(R.id.bluetooth).setIcon(ru.profitsw2000.core.R.drawable.bt_on)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        requireActivity().invalidateOptionsMenu()
+        //requireActivity().invalidateOptionsMenu()
         return when (item.itemId) {
             R.id.connect -> {
-                //Toast.makeText(requireActivity(), "Connect to device", Toast.LENGTH_SHORT).show()
-                isConnected = !isConnected
                 true
             }
             R.id.bluetooth -> {
-                //Toast.makeText(requireActivity(), "Switch bluetooth on/off", Toast.LENGTH_SHORT).show()
-                bluetoothIsOn = !bluetoothIsOn
+                bluetoothOperation()
                 true
             }
             else -> true
@@ -63,8 +96,9 @@ class UpdateTimeFragment : Fragment() {
     override fun onPrepareOptionsMenu(menu: Menu) {
         super.onPrepareOptionsMenu(menu)
 
-        if (bluetoothIsOn)
-            menu.findItem(R.id.bluetooth).setIcon(ru.profitsw2000.core.R.drawable.bt_on)
+        if (bluetoothPermissionIsGranted())
+            if (bluetoothAdapter.isEnabled) menu.findItem(R.id.bluetooth).setIcon(ru.profitsw2000.core.R.drawable.bt_on)
+            else menu.findItem(R.id.bluetooth).setIcon(ru.profitsw2000.core.R.drawable.bt_off)
         else
             menu.findItem(R.id.bluetooth).setIcon(ru.profitsw2000.core.R.drawable.bt_off)
 
@@ -72,6 +106,99 @@ class UpdateTimeFragment : Fragment() {
             menu.findItem(R.id.connect).setIcon(ru.profitsw2000.core.R.drawable.connected)
         else
             menu.findItem(R.id.connect).setIcon(ru.profitsw2000.core.R.drawable.unconnected)
+    }
+
+    private fun bluetoothOperation() {
+        if (bluetoothAdapter == null) {
+            MaterialAlertDialogBuilder(requireActivity())
+                .setTitle("Ошибка включения Bluetooth")
+                .setMessage("Bluetooth на данном устройстве отсутствует")
+                .setPositiveButton("OK") { dialog, _ -> dialog.dismiss()}
+                .create()
+                .show()
+        } else {
+            if (VERSION.SDK_INT > VERSION_CODES.R) {
+                getBluetoothPermission()
+            } else {
+                switchBluetooth()
+            }
+        }
+    }
+
+    private fun getBluetoothPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                requireActivity(),
+                android.Manifest.permission.BLUETOOTH_CONNECT
+            ) == PackageManager.PERMISSION_GRANTED -> switchBluetoothOn()
+
+            //////////////////////////////////////////////////////////////////
+
+            shouldShowRequestPermissionRationale(android.Manifest.permission.BLUETOOTH_CONNECT) -> showRationaleDialog()
+
+            //////////////////////////////////////////////////////////////////
+
+            else -> requestPermissionLauncher.launch(android.Manifest.permission.BLUETOOTH_CONNECT)
+        }
+    }
+
+    private fun showRationaleDialog() {
+        MaterialAlertDialogBuilder(requireActivity())
+            .setTitle("Разрешение Bluetooth")
+            .setMessage("При подключении к настенным часам используется Bluetooth. Для синхронизации отображаемого на настенных часах времени " +
+                    "необходимо предоставить приложению разрешение на использование Bluetooth.")
+            .setNegativeButton("OK") { dialog, _ -> dialog.dismiss() }
+            .create()
+            .show()
+    }
+
+    private fun showBluetoothEnablingDialog() {
+        MaterialAlertDialogBuilder(requireActivity())
+            .setTitle("Включение Bluetooth")
+            .setMessage("Для синхронизации отображаемого на настенных часах времени необходимо включить Bluetooth.")
+            .setNegativeButton("OK") { dialog, _ -> dialog.dismiss() }
+            .create()
+            .show()
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private fun switchBluetooth() {
+        if (bluetoothAdapter.isEnabled && VERSION.SDK_INT <= VERSION_CODES.R) {
+            bluetoothAdapter.disable()
+        } else {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, requestCodeForEnable)
+        }
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private fun switchBluetoothOn() {
+        if (!bluetoothAdapter.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, requestCodeForEnable)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == requestCodeForEnable) {
+            if (resultCode == Activity.RESULT_OK) {
+                requireActivity().invalidateOptionsMenu()
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                showBluetoothEnablingDialog()
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun bluetoothPermissionIsGranted(): Boolean {
+        return if (VERSION.SDK_INT > VERSION_CODES.R) {
+            ContextCompat.checkSelfPermission(
+                    requireActivity(),
+                    android.Manifest.permission.BLUETOOTH_CONNECT
+                ) == PackageManager.PERMISSION_GRANTED
+        } else true
     }
 
     companion object {
