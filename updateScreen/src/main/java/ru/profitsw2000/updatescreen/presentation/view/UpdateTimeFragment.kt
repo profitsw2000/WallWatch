f415339058d2b830e09e8e789f98c534b35571e9
profitsw2000 <profitsw2000@gmail.com>
2025-03-30 17:33:47 +0300

    Сделано определение изменение состояния блютуз (включение/выключение) с использованием приёмника широковещательных сообщений и изменение иконки в меню в соответствии с состоянием блютуз.

--- updateScreen/src/main/java/ru/profitsw2000/updatescreen/presentation/view/UpdateTimeFragment.kt
+++ updateScreen/src/main/java/ru/profitsw2000/updatescreen/presentation/view/UpdateTimeFragment.kt
@@ -4,7 +4,9 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Build.VERSION
@@ -18,17 +20,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import ru.profitsw2000.core.utils.bluetoothbroadcastreceiver.BluetoothStateBroadcastReceiver
+import ru.profitsw2000.core.utils.bluetoothbroadcastreceiver.OnBluetoothStateListener
 import ru.profitsw2000.updatescreen.R
 import ru.profitsw2000.updatescreen.databinding.FragmentUpdateTimeBinding
 
-class UpdateTimeFragment : Fragment() {
+class UpdateTimeFragment : Fragment(), OnBluetoothStateListener {
 
     private var _binding: FragmentUpdateTimeBinding? = null
     private val binding get() = _binding!!
-    private var bluetoothIsOn = false
+    private val bluetoothStateBroadcastReceiver = BluetoothStateBroadcastReceiver(this)
     private var isConnected = false
     private val bluetoothManager: BluetoothManager by lazy {
         requireActivity().getSystemService(BluetoothManager::class.java)
@@ -41,15 +46,21 @@ class UpdateTimeFragment : Fragment() {
             ActivityResultContracts.RequestPermission()
         ) { isGranted: Boolean ->
             if (isGranted) {
-                switchBluetooth()
+                if (VERSION.SDK_INT > VERSION_CODES.S) switchBluetoothOn()
+                else switchBluetooth()
             } else {
                 showRationaleDialog()
             }
         }
 
+    @RequiresApi(VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
+        requireActivity().registerReceiver(
+            bluetoothStateBroadcastReceiver,
+            IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED)
+        )
     }
 
     override fun onCreateView(
@@ -59,18 +70,6 @@ class UpdateTimeFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentUpdateTimeBinding.bind(inflater.inflate(R.layout.fragment_update_time, container, false))
         return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
-
-    override fun onStart() {
-        super.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -97,8 +96,10 @@ class UpdateTimeFragment : Fragment() {
         super.onPrepareOptionsMenu(menu)
 
         if (bluetoothPermissionIsGranted())
-            if (bluetoothAdapter.isEnabled) menu.findItem(R.id.bluetooth).setIcon(ru.profitsw2000.core.R.drawable.bt_on)
-            else menu.findItem(R.id.bluetooth).setIcon(ru.profitsw2000.core.R.drawable.bt_off)
+            if (bluetoothAdapter.isEnabled)
+                menu.findItem(R.id.bluetooth).setIcon(ru.profitsw2000.core.R.drawable.bt_on)
+            else
+                menu.findItem(R.id.bluetooth).setIcon(ru.profitsw2000.core.R.drawable.bt_off)
         else
             menu.findItem(R.id.bluetooth).setIcon(ru.profitsw2000.core.R.drawable.bt_off)
 
@@ -130,7 +131,9 @@ class UpdateTimeFragment : Fragment() {
             ContextCompat.checkSelfPermission(
                 requireActivity(),
                 android.Manifest.permission.BLUETOOTH_CONNECT
-            ) == PackageManager.PERMISSION_GRANTED -> switchBluetoothOn()
+            ) == PackageManager.PERMISSION_GRANTED ->
+                if (VERSION.SDK_INT > VERSION_CODES.S) switchBluetoothOn()
+                else switchBluetooth()
 
             //////////////////////////////////////////////////////////////////
 
@@ -201,8 +204,17 @@ class UpdateTimeFragment : Fragment() {
         } else true
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        requireActivity().unregisterReceiver(bluetoothStateBroadcastReceiver)
+    }
+
     companion object {
         @JvmStatic
         fun newInstance() = UpdateTimeFragment()
+    }
+
+    override fun onBluetoothStateChanged() {
+        requireActivity().invalidateOptionsMenu()
     }
 }
